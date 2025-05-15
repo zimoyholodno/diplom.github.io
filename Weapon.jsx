d5b8f8efb8c032a401437d36125633fd8d0c6ae1
ponyal <128210461+zimoyholodno@users.noreply.github.com>
2025-05-16 00:42:34 +0400

    fix: нормальный репозиторий

--- Weapon.jsx
+++
@@ -1,9 +0,0 @@
-import {WeaponModel} from "./WeaponModel.jsx";
-
-export const Weapon = (props) => {
-    return (
-        <group {...props}>
-            <WeaponModel />
-        </group>
-    );
-}
